fab3c545d0870e293e05aa280ae27b222214241a
dkile <dkile532@naver.com>
2023-10-05 20:43:50 +0900

    컬러 팔레트 생성 (#7)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        "gray-light": "#B4B4B4",
+        gray: "#5A5A5A",
+        "gray-dark": "#1D1D1D",
+        pink: "#FB27FF",
+        cyan: "#27F2FF",
+      },
+    },
+  },
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
