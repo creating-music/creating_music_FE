7b9d39774c6a9f8a165d70c1b55708c93b1dd1c1
dkile <dkile532@naver.com>
2023-11-13 21:43:18 +0900

    Fix: Icon 컴포넌트 에러 수정 (#15)

--- app/_components/Icon.tsx
+++ app/_components/Icon.tsx
@@ -7,7 +7,19 @@ interface IconProps extends HTMLAttributes<HTMLSpanElement> {
 const BASE_URL = "https://creating-music.s3.ap-northeast-2.amazonaws.com/icons";
 
 const iconMatcher = {
-  airplay: `${BASE_URL}/airplay`,
+  airplay: `${BASE_URL}/airplay.svg`,
+  download: `${BASE_URL}/download.svg`,
+  edit: `${BASE_URL}/edit-2.svg`,
+  equalizer2: `${BASE_URL}/equalizer-2.svg`,
+  equalizer: `${BASE_URL}/equalizer.svg`,
+  eyeoff: `${BASE_URL}/eye-off.svg`,
+  eyeon: `${BASE_URL}/eye.svg`,
+  heart: `${BASE_URL}/heart.svg`,
+  music: `${BASE_URL}/music.svg`,
+  pause: `${BASE_URL}/pause.svg`,
+  play: `${BASE_URL}/play.svg`,
+  share: `${BASE_URL}/share-2.svg`,
+  trash: `${BASE_URL}/trash-2.svg`,
 };
 
 export type IconName = keyof typeof iconMatcher;
@@ -15,7 +27,7 @@ export type IconName = keyof typeof iconMatcher;
 export default async function Icon({ name, ...props }: IconProps) {
   const iconUrl = iconMatcher[name];
   const res = await fetch(iconUrl);
-  if (!res.ok) throw new Error("Fetching icon failed.");
+  if (!res.ok) console.error(res);
 
   const icon = await res.text();
 
