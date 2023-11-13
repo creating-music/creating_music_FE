f413ba6772dbf9fd2f4884fcde02035c8e2a7803
dkile <dkile532@naver.com>
2023-11-13 21:43:08 +0900

    Feature/dkile/3-메인 페이지 뷰 구현 (#12)
    * routes.ts에 뮤직 리스트 경로 추가
    
    * 레이아웃 생성
    
    * 루트 라우트에서 music/listen 페이지로 리다이렉션 설정
    
    * 상단 배너 생성
    
    * Feat: 컨텐츠 프레임, 래퍼 UI 구현
    
    * Refactor: Frame, Wrapper 컴포넌트 ui 폴더로 이동
    
    * Refactor: Frame, Wrapper 컴포넌트 app/_component로 이동
    
    * Refactor: Wrapper 컴포넌트를 Frame으로 통합
    
    * Feat: 홈페이지 뷰 구현
    
    * Refactor: Music type 추출
    
    * Refactor: MusicList 컴포넌트 추출

---
+++ app/music/list/_components/MusicBar.tsx
@@ -0,0 +1,47 @@
+import Image from "next/image";
+import { Music } from "@/app/music/list/types";
+
+interface Props {
+  music: Music;
+}
+
+export default function MusicBar({ music }: Props) {
+  return (
+    <div className="bg-ugray-500 flex h-[7.5rem] w-[75rem] px-[1.5rem] py-[1.25rem]">
+      <div className="flex h-[5rem] w-[5rem] items-center justify-center text-[1.125rem]">
+        {music.rank}
+      </div>
+      <div className="flex h-[5rem] w-[5rem] items-center justify-center">
+        {music.thumbnail ? (
+          <Image alt="" src={music.thumbnail} fill />
+        ) : (
+          <span>music</span>
+        )}
+      </div>
+      <div className="flex h-[5rem] w-[15rem] flex-col justify-center px-[2rem] text-center">
+        <h3 className="line-clamp-1 w-full overflow-ellipsis">{music.title}</h3>
+        <small className="line-clamp-1 overflow-ellipsis text-u-gray-300">
+          {music.author}
+        </small>
+      </div>
+      <div className="line-clamp-1 flex h-[5rem] w-[5rem] items-center justify-center overflow-ellipsis text-u-gray-200">
+        {music.genre}
+      </div>
+      <div className="flex h-[5rem] w-[5rem] items-center justify-center">
+        play
+      </div>
+      <div className="flex h-[5rem] w-[5rem] items-center justify-center text-u-gray-200">
+        {music.time}
+      </div>
+      <div className="flex h-[5rem] w-[17.5rem] items-center justify-center text-u-gray-200">
+        progress bar
+      </div>
+      <div className="flex h-[5rem] w-[13rem] items-center justify-center gap-[2rem] text-u-gray-200">
+        <span className="h-[1.5rem] w-[1.5rem]">1</span>
+        <span className="h-[1.5rem] w-[1.5rem]">2</span>
+        <span className="h-[1.5rem] w-[1.5rem]">3</span>
+        <span className="h-[1.5rem] w-[1.5rem]">4</span>
+      </div>
+    </div>
+  );
+}
